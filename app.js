fa6e01101b8832ad4cab505c07de05b176713fcf
Dipankar Das <ddipankar@vmware.com>
2020-06-10 18:28:56 -0700

    Body Parser added. Post Support

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 const express = require('express')
+const bodyParser = require("body-parser");
 const app = express()
 const port = process.env.PORT || 3000
 
-//app.set('port', process.env.PORT || port)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('VMWare CPQ Expression Processor');
 })
 
-// PLUS: %2b    -->  2 %2b 3 = 5
-// MINUS: -    -->  3 - 2 = 1
-// MULTIPLY: *  --> 2 * 3 = 6
-// DIVIDE: /    --> 4 / 2 = 1
-// EQUALS: %3d%3d -->  4 %3d%3d 4 = true
-// BRACKET: eval?expr=(5-2)*6 --> {"input":"(5-2)*6","result":18}
-// STRING: eval?expr="Hello"%3d%3d"Hello" --> {"input":"\"Hello\"==\"Hello\"","result":true}
 app.get('/eval', function (req, res) {
     let expr = req.query.expr;
     console.log(expr);
@@ -23,7 +18,13 @@ app.get('/eval', function (req, res) {
 })
 
 app.post('/', function (req, res) {
-    res.send('TBD: Eval a POST request');
+    let jsonData = req.body;
+    //console.log(jsonData);
+    Object.keys(jsonData).forEach(function(key) {
+        jsonData[key] = eval(jsonData[key]);
+    });
+    //console.log(jsonData);
+    res.send(jsonData);
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Expression Eval App is listening at http://localhost:${port}`))
